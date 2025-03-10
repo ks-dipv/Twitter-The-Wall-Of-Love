12da365e2f5daf80b02ecb832f195d752768e477
ks-mansi-r <mansi.rupareliya@kalkani.in>
2025-03-10 18:15:26 +0530

    enablecors

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -42,6 +42,10 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   // Initialize Passport
   app.use(passport.initialize());
   app.use(passport.session());
