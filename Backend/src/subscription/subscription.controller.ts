017ee207d1987c11cf1f0612c4fab80eec8e5324
ks-mansi-r <mansi.rupareliya@kalkani.in>
2025-04-14 10:29:01 +0530

    webhook

--- Backend/src/subscription/subscription.controller.ts
+++ Backend/src/subscription/subscription.controller.ts
@@ -1,7 +1,8 @@
 import { Controller } from '@nestjs/common';
-import { Post, Param, Req } from '@nestjs/common';
+import { Post, Param, Body,Req, Headers, BadRequestException } from '@nestjs/common';
 import { SubscriptionService } from './service/subscription.service';
 import { User } from 'src/common/decorator/user.decorater';
+import Stripe from 'stripe';
 @Controller('subscription')
 export class SubscriptionController {
 
@@ -14,6 +15,28 @@ export class SubscriptionController {
       @Param('planId') planId: number,
     ) {
       return this.subscriptionService.createCheckoutSession(user, planId);
+    }
+
+    @Post('webhook')
+    async handleStripeWebhook(
+      @Body() body: any,
+      @Headers('stripe-signature') sig: string,
+    ) {
+      const webhookSecret =
+        this.subscriptionService.getStripeWebhookSecret();
+  
+      let event: Stripe.Event;
+  
+      try {
+        event = this.subscriptionService.verifyStripeWebhook(body, sig, webhookSecret);
+      } catch (err) {
+        console.log('Webhook Error:', err.message);
+        throw new BadRequestException(`Webhook Error: ${err.message}`);
+      }
+  
+      await this.subscriptionService.handleWebhookEvent(event);
+  
+      return { received: true };
     }
     
 }
